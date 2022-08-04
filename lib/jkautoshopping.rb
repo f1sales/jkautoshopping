606fffc13ef4f35f1f47cb0c742caf0b0cbdd82c
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-08-04 15:00:12 -0300

    Add website source name

--- lib/jkautoshopping.rb
+++ lib/jkautoshopping.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 require_relative "jkautoshopping/version"
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Jkautoshopping
   class Error < StandardError; end
-  # Your code goes here...
+
+  class F1SalesCustom::Email::Source
+    def self.all
+      [
+        {
+          email_id: 'website',
+          name: 'Website'
+        }
+      ]
+    end
+  end
 end
--- spec/jkautoshopping_spec.rb
+++ spec/jkautoshopping_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Jkautoshopping do
   it "has a version number" do
     expect(Jkautoshopping::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
