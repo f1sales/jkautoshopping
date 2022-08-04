1a66b49e58c2af20208966db96e20389d2d6dd90
Nathanael Vieira <nathanael@f1sales.com.br>
2022-08-04 11:46:39 -0300

    Git init

---
+++ lib/jkautoshopping.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jkautoshopping/version"
+
+module Jkautoshopping
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jkautoshopping_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jkautoshopping do
+  it "has a version number" do
+    expect(Jkautoshopping::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
